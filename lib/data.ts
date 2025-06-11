5a07894568ae5c7ae6ec00402de22c9c41b71d3c
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-06-11 17:20:57 -0600

    Add judgingPanelQuery

--- lib/data.ts
+++ lib/data.ts
@@ -66,13 +66,14 @@ export async function fetchEventById(eventId: number): Promise<EventDetails | nu
          `;
 
          const judgingPanelQuery = `
-            SELECT rd.event_id, rd.division_id, rd.division_name, rd.round_id
+            SELECT rd.event_id, rd.division_id, rd.division_name, rd.round_id, hd.round_heat_id, ej.personnel_id
             FROM ss_round_details rd JOIN ss_heat_details hd ON rd.round_id = hd.round_id
             JOIN ss_heats_results hr ON hr.round_heat_id = hd.round_heat_id
-            JOIN ss_event_divisions ed ON ed.division_id = rd.division_id;
+            JOIN ss_event_divisions ed ON ed.division_id = rd.division_id            JOIN ss_event_judges ej ON rd.event_id = ej.event_id
+            WHERE ej.event_id = $1;
          `;
          // TO FINISH
-         // judgingPanelQuery + judgeQuery to create the Judging Panel and to create the QRCodes
+         // judgingPanelQuery + judgeQuery to create the Judging Panel and to create the QRCodes > then map it to create the pages, can we create the pages from a mapping function?
 
          const [divisionResult, athleteResult, judgeResult, headJudgeResult] = await Promise.all([
              client.query<Division>(divisionQuery, [eventId]),
