e64007ac8d2fe28f6e1b61d72dade839aae9796f
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-07-27 12:24:47 -0600

    Use generic instead to facilitate SLL variable assignments

--- components/manage-round-heat/roundSLL.ts
+++ components/manage-round-heat/roundSLL.ts
@@ -1,31 +1,15 @@
-export class roundNode<T> {
-  public round_id: string;
-  public round_name: string;
-  public num_athletes: number;
-  public num_heats: number;
-  public round_num: number;
-  public division_id: number;
-  public division_name: string;
-  public next: roundNode<T> | null;
+import roundNode from "@/components/manage-round-heat/roundNode";
 
-  constructor(
-    round_id: string,
-    round_name: string,
-    num_athletes: number,
-    num_heats: number,
-    round_num: number,
-    division_id: number,
-    division_name: string,
-    next: roundNode<T> 
-    | null = null) {
+export class SinglyLinkedList<roundNode> {
+  private head: roundNode | null = null;
+  private tail: roundNode | null = null;
 
-        this.round_id = round_id;
-        this.next = next;
-        this.round_name = round_name;
-        this. num_athletes = num_athletes;
-        this.num_heats = num_heats;
-        this.round_num = round_num;
-        this.division_id = division_id;
-        this.division_name = division_name;
-    }
+  public isEmpty(): boolean {
+    return this.head === null;
+  }
+
+  public clear(): void {
+    this.head = this.tail = null;
+  }
+
 }
